f51ea5378bfdd52171ab7dc9dbac5a095de0531b
Pedro <pedro-gomes17@hotmail.com>
2022-05-03 08:13:08 -0300

    config: initial project config

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  )
+}
---
+++ src/components/Widget.tsx
@@ -0,0 +1,8 @@
+export function Widget() {
+  return (
+    <div>
+      <h1>Widgets</h1>
+      <Widget />
+    </div>
+  )
+}

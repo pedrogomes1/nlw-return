9074dff979d202502cbb8583ed71989e4fccfcc1
Pedro <pedro-gomes17@hotmail.com>
2022-05-04 20:02:53 -0300

    feat: create new route to post a new feedback with email sent

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,46 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
 
 const app = express();
+
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "031d82b7e08f68",
+    pass: "f345a0e1527d4b"
+  }
+});
+
+app.post('/feedbacks', async (req, res) => {
+  const { comment, type, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      comment,
+      type,
+      screenshot,
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Pedro <pedoguinha2@gmail.com',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>` 
+    ].join('\n')
+
+  })
+
+  return res.status(201).json({ data: feedback });
+})
 
 app.listen(3333, () => {
   console.log('HTTP server running!')
